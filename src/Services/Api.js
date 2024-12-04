765455128bdc2132159de5ec78798a4111b73f50
shiju k <106902741+123shiju@users.noreply.github.com>
2024-12-05 02:07:57 +0530

    Update Api.js

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -2,29 +2,33 @@ import axios from "axios";
 
 const API_URL = "https://userauthserver.vercel.app";
 
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true, 
+
 export const register = (userData) => {
-  return axios.post(`${API_URL}/register`, userData);
+  return axiosInstance.post(`${API_URL}/register`, userData);
 };
 
 export const login = (credentials) => {
-  return axios.post(`${API_URL}/login`, credentials);
+  return axiosInstance.post(`${API_URL}/login`, credentials);
 };
 
 export const getUsers = () => {
-  return axios.get(`${API_URL}/getuser`);
+  return axiosInstance.get(`${API_URL}/getuser`);
 };
 
 export const deleteUser = (id) => {
-  return axios.delete(`${API_URL}/deleteuser/${id}`);
+  return axiosInstance.delete(`${API_URL}/deleteuser/${id}`);
 };
 
 export const updateUser = (id, updatedData) => {
-  return axios.put(`${API_URL}/updateuser/${id}`, updatedData);
+  return axiosInstance.put(`${API_URL}/updateuser/${id}`, updatedData);
 };
 
 export const getUserById = async (id) => {
   try {
-    const response = await axios.get(`${API_URL}/getuserById/${id}`);
+    const response = await axiosInstance.get(`${API_URL}/getuserById/${id}`);
     return response.data;
   } catch (error) {
     throw error.response?.data || { error: "Failed to fetch user details" };
